d71e1c5c7910f2f6efe0131833d35d440372233e
maureen <maureenndindamutua@gmail.com>
2026-04-28 09:13:26 +0300

    fix syntax error and improve Akan name generation logic

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 document.getElementById("formAkan"). addEventListener("submit", function(e) {
     e.preventDefault();
-}
+
 let day = parseInt(document.getElementById("day").value);
 let month = parseInt(document.getElementById("month").value);
 let year = parseInt(document.getElementById("year").value);
@@ -22,4 +22,33 @@ if (!gender) {
     alert("Please select gender");
 }
 
-let maleNames = ["Kwasi", "K"]
+let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+let CC = Math.floor(year / 100);
+let YY = year % 100;
+let MM = month;
+let DD = day;
+
+let dayofWeek = Math.floor(
+    (((CC / 4) - (2 * CC) - 1) + ((5 * YY) / 4) + ((26 * (MM + 1)) / 10) + DD) % 7 
+);
+
+if (dayofWeek < 0) {
+    dayofWeek = (dayofWeek + 7) % 7;
+}
+
+let akanName;
+
+if (gender === "male") {
+    akanName = maleNames[dayofWeek];
+} else {
+    akanName = femaleNames[dayofWeek];
+}
+
+result.textContent = "Your Akan name is: " +akanName;
+});
+
+
+
+
